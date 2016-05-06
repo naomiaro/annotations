041d61be5e946bb56c89221fcc346e5fcd8811ce
Naomi Aro <naomiaro@gmail.com>
2016-05-06 19:00:13 +0100

    Use moment duration formatting

--- src/TimedAnnotationList.js
+++ src/TimedAnnotationList.js
@@ -2,10 +2,14 @@ import h from 'virtual-dom/h';
 import diff from 'virtual-dom/diff';
 import patch from 'virtual-dom/patch';
 
+import moment from 'moment';
+import 'moment-duration-format';
+
 export default class {
 
     constructor() {
         this.notes = [];
+        this.durationFormat = 'h:mm:ss.S'
     }
 
     /*
@@ -23,8 +27,12 @@ export default class {
         this.notes = notes;
     }
 
+    formatDuration(duration) {
+        return moment.duration(Number(duration), 'seconds').format(this.durationFormat, {trim: false});
+    }
+
     render() {
-        return h('div.container', this.notes.map(function(note) {
+        return h('div.container', this.notes.map((note) => {
             return h('div.row', [
                 h('col-xs-1.col-sm-1.col-md-1.col-lg-1', [
                     h('div.box', note.id)
@@ -33,7 +41,7 @@ export default class {
                     h('div.box', note.lines)
                 ]),
                 h('col-xs-2.col-sm-2.col-md-2.col-lg-2', [
-                    h('div.box', note.begin)
+                    h('div.box', this.formatDuration(note.begin))
                 ])
             ]);
         }));
